5901580fa250525dc9d681d68809d2e32384bdfd
luis angel pfuño canales <alguien310599@gmail.com>
2021-05-14 10:41:54 -0500

    v0.1.1

--- 07.script.sh
+++ 07.script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf /usr/share/zoneinfo/America/Lima /etc/localtime
 hwclock --systohc
-cp locale.gen /etc/
+cp locale.gen /etc/locale.gen
 locale-gen
 echo KEYMAP=es > /etc/vconsole.conf
 echo LANG=es_PE.UTF8 > /etc/locale.conf
